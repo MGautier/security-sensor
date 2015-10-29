797c72570c3e7a238cae72e7651ce8adc1382faf
gmacia <gmacia@metis.ugr.es>
2015-10-29 15:41:33 +0100

    Separado codigo de clase Firewall en firewall.py

--- trunk/version-1-0/controller.py
+++ trunk/version-1-0/controller.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from source import Firewall
+from firewall import Firewall
 from datetime import date
 
 
 # Creamos un objeto del tipo Source y operamos con él
 
-input_source = {'T': 'Firewall', 'M': 'iptables', 'P' : 'source.log', 'Version' : 'v1.4.21', 'Active' : '1', 'Software_class' : 'Firewall', 'Path' : '/var/log/iptables.log'}
+input_source = {'T': 'Firewall', 'M': 'iptables', 'P' : 'source.log', 
+'Version' : 'v1.4.21', 'Active' : '1', 'Software_class' : 'Firewall', 
+'Path' : '/var/log/iptablesmini.log'}
 
-test = Firewall('proyecto_bd', args=(1,), source={'T' : 'Firewall', 'M' : 'iptables', 'P' : '/var/log/iptables.log'})
+test = Firewall('proyecto_bd', args=(1,), source={'T' : 'Firewall', 'M' 
+: 'iptables', 'P' : './iptablesmini.log'})
 lista = []
 test.start()
 #print test.processing()
-lista = test.join()
+#lista = test.join()
 
 #print date.today().year
 #print lista[0]
