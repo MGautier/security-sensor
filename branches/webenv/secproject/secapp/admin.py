172b272eb97a1dc30407d10c7ab86223edbeabfa
MGautier <moisesgautiergomez@correo.ugr.es>
2015-12-08 18:03:29 +0100

    Eliminando la app django y comenzando de nuevo

--- branches/webenv/secproject/secapp/admin.py
+++ branches/webenv/secproject/secapp/admin.py
@@ -1,13 +1,3 @@
 from django.contrib import admin
-from .models import Ips, Events, Ports, LogSources, PacketAdditionalInfo, PacketEventsInformation, Macs, Tags
 
 # Register your models here.
-
-admin.site.register(Ips)
-admin.site.register(Events)
-admin.site.register(Ports)
-admin.site.register(LogSources)
-admin.site.register(PacketAdditionalInfo)
-admin.site.register(PacketEventsInformation)
-admin.site.register(Macs)
-admin.site.register(Tags)
--- branches/webenv/secproject/secproject/settings.py
+++ branches/webenv/secproject/secproject/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'gunicorn',
-    'secapp',
 ]
 
 MIDDLEWARE_CLASSES = [
