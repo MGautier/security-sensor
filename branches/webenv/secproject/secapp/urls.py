716c25836a3dafd4ba5a9b9c0f10c303712cca2a
MGautier <moisesgautiergomez@correo.ugr.es>
2015-12-16 22:19:03 +0100

    Añadiendo nuevas vistas de ejemplo al proyecto siguiendo el tutorial de django

--- branches/webenv/secproject/secapp/urls.py
+++ branches/webenv/secproject/secapp/urls.py
@@ -3,5 +3,15 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # Index view
     url(r'^$', views.index, name='index'),
+
+    # Events description
+    url(r'^(?P<id_event>[0-9]+)/$', views.events, name='events'),
+
+    # Log sources description
+    url(r'^(?P<id_source>[0-9]+)/sources/$', views.log_sources, name='log_sources'),
+
+    # Ips description
+    url(r'^(?P<id_ip>[0-9]+)/ips/$', views.ips, name='ips'),
 ]
--- branches/webenv/secproject/secapp/views.py
+++ branches/webenv/secproject/secapp/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 
 def index(request):
     return HttpResponse("Hello, world. You're at the secapp index.")
+
+
+def events(request, id_event):
+    return HttpResponse("You're looking at event %s. " % id_event)
+
+
+def log_sources(request, id_source):
+    response = "You're looking at the log_source of event %s. "
+    return HttpResponse(response % id_source)
+
+
+def ips(request, id_ip):
+    return HttpResponse("You're looking at the ips of packet events %s. " % id_ip)
