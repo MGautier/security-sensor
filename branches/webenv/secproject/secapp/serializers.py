5214b6e820eb82e42bc7e807ecf5c32f8fec9643
MGautier <moisesgautiergomez@correo.ugr.es>
2016-02-02 22:34:27 +0100

    Cambiando el nombre de la clase serializer a Events

--- branches/webenv/secproject/secapp/serializers.py
+++ branches/webenv/secproject/secapp/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from models import Events
 
 
-class SecappSerializer(serializers.ModelSerializer):
+class EventsSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Events
