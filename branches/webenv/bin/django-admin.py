92cdae8333de18bcd6c9b641f4f555ecc721ad82
MGautierGomez <moisesgautiergomez@correo.ugr.es>
2016-02-28 18:21:57 +0100

    Eliminando los archivos del virtualenv y de la compilación de nodejs/npm

--- branches/webenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/debian-moises/Documentos/securityproject/branches/webenv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
