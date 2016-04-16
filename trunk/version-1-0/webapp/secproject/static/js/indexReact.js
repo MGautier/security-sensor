b68720d6f4d89b8b17a49ce437e1ce982ed252b2
MGautier <moisesgautiergomez@correo.ugr.es>
2016-04-16 23:15:32 +0200

    New features of reactjs using c3js to generate a chart and then reload by reactjs

--- trunk/version-1-0/webapp/secproject/static/js/indexReact.js
+++ trunk/version-1-0/webapp/secproject/static/js/indexReact.js
@@ -1,34 +1,162 @@
-var chart = c3.generate({
-    data: {
-        columns: [
-            ['data1', 300, 350, 300, 0, 0, 0],
-            ['data2', 130, 100, 140, 200, 150, 50]
-        ],
-        types: {
-            data1: 'area',
-            data2: 'area-spline'
-        }
-    }
-});
+/*setTimeout(function () {
+ c3.generate({
+ data: {
+ url: 'api/visualizations/week.json',
+ mimeType: 'json',
+ keys: {
+ x: 'Name_Day',
+ value: ['Process_Events']
+ }
+ },
+ axis: {
+ x: {
+ type: 'category'
+ }
+ }
+ });
+ }, 1000);*/
 
+/*var chart = c3.generate({
+ data: {
+ url: [
+ ['data1', 300, 350, 300, 0, 0, 0],
+ ['data2', 130, 100, 140, 200, 150, 50]
+ ],
+ types: {
+ data1: 'area',
+ data2: 'area-spline'
+ }
+ }
+ });*/
+
+
+/*var Visualization = React.createClass({
+ render: function() {
+ return (
+ <div className="visualization">
+ <div className="visualizationDate">
+ {this.props.Date}
+ </div>
+ <div className="visualizationDay">
+ {this.props.Day}
+ </div>
+ <div className="visualizationHour">
+ {this.props.Hour}
+ </div>
+ <div className="visualizationEvents">
+ {this.props.Events}
+ </div>
+ </div>
+ );
+ }
+ });*/
 
 var Visualization = React.createClass({
   render: function() {
-    return (
-        <div className="visualization">
-        <div className="visualizationDate">
-        {this.props.Date}
-      </div>
-        <div className="visualizationDay">
-        {this.props.Day}
-      </div>
-        <div className="visualizationHour">
-        {this.props.Hour}
-      </div>
-        <div className="visualizationEvents">
-        {this.props.Events}
-      </div>
-        </div>
+    return(
+      c3.generate({
+        data: {
+          url: 'api/visualizations/chart.json/',
+          mimeType: 'json',
+          selection: {
+            enabled: true
+          },
+          onclick: function (d, data, element){
+
+            var JSON = React.createClass({
+              render: function() {
+                return (<Event url="api/visualizations/chart.json"/>);
+              }
+            });
+
+            var Event = React.createClass({
+              render: function() {
+                return (
+                    <div className="event">
+                    <div className="eventTimestamp">
+                    {this.props.Timestamp}
+                  </div>
+                    <div className="eventComment">
+                    {this.props.Comment}
+                  </div>
+                    </div>
+                );
+              }
+            });
+
+            var EventsComponent = React.createClass({
+              loadEventsFromServer: function(){
+                $.ajax({
+                  url: this.props.url,
+                  dataType: 'json',
+                  cache: false,
+                  success: function(data) {
+                    this.setState({data: data});
+                  }.bind(this),
+                  error: function(xhr, status, err){
+                    console.error(this.props.url, status, err.toString());
+                  }.bind(this)
+                });
+              },
+              getInitialState: function(){
+                return {data: []};
+              },
+              componentDidMount: function(){
+                this.loadEventsFromServer();
+                setInterval(this.loadEventsFromServer, this.props.pollInterval);
+              },
+              render: function(){
+                var testStyle = { fontSize: '18px', marginRight: '20px' };
+                return (
+                    <div className="eventsComponent" style={testStyle}>
+                    <h1>Events</h1>
+                    <EventsList data={this.state.data}/>
+                    </div>
+                );
+              }
+            });
+
+            var EventsList = React.createClass({
+              render: function(){
+                var eventNodes = this.props.data.map(function(event){
+                  return (
+                      <li><Event Timestamp={event.Local_Timestamp} Comment={event.Comment} key={event.id} /></li>
+                  );
+                });
+                return (
+                    <div className="eventsList">
+                    <ul>
+                    {eventNodes}
+                  </ul>
+                    </div>
+                );
+              }
+            });
+
+            ReactDOM.render(
+              console.log("ON-RENDER",d),
+                <EventsComponent url="api/events/" pollInterval={10000} />,
+              document.getElementById('sub-content')
+            );
+          },
+          keys: {
+            x: 'day',
+            value: ['events']
+          },
+          names: {
+            events: 'Iptables Events'
+          },
+          type: 'area-spline'
+        },
+        subchart: {
+          show: true
+        },
+        axis: {
+          x: {
+            type: 'category'
+          }
+        }
+      })
     );
   }
 });
@@ -82,72 +210,10 @@ var VisualizationsList = React.createClass({
   }
 });
 
-var Event = React.createClass({
-  render: function() {
-    return (
-        <div className="event">
-        <div className="eventTimestamp">
-        {this.props.Timestamp}
-      </div>
-        <div className="eventComment">
-        {this.props.Comment}
-      </div>
-        </div>
-    );
-  }
-});
 
-var EventsComponent = React.createClass({
-  loadEventsFromServer: function(){
-    $.ajax({
-      url: this.props.url,
-      dataType: 'json',
-      cache: false,
-      success: function(data) {
-        this.setState({data: data});
-      }.bind(this),
-      error: function(xhr, status, err){
-        console.error(this.props.url, status, err.toString());
-      }.bind(this)
-    });
-  },
-  getInitialState: function(){
-    return {data: []};
-  },
-  componentDidMount: function(){
-    this.loadEventsFromServer();
-    setInterval(this.loadEventsFromServer, this.props.pollInterval);
-  },
-  render: function(){
-    var testStyle = { fontSize: '18px', marginRight: '20px' };
-    return (
-        <div className="eventsComponent" style={testStyle}>
-        <h1>Events</h1>
-        <EventsList data={this.state.data}/>
-        </div>
-    );
-  }
-});
 
-var EventsList = React.createClass({
-  render: function(){
-    var eventNodes = this.props.data.map(function(event){
-      return (
-          <li><Event Timestamp={event.Local_Timestamp} Comment={event.Comment} key={event.id} /></li>
-      );
-    });
-    return (
-        <div className="eventsList">
-        <ul>
-        {eventNodes}
-      </ul>
-        </div>
-    );
-  }
-});
-
-React.render(
-    //<EventsComponent url="api/events/" pollInterval={10000} />,
-    <VisualizationsComponent url="api/visualizations/week" pollInterval={10000} />,
+ReactDOM.render(
+  //<EventsComponent url="api/events/" pollInterval={10000} />,
+    <VisualizationsComponent url="api/visualizations/week/" pollInterval={60000} />,
   document.getElementById('content')
 );
