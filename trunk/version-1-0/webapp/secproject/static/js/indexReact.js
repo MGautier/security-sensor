021e9f2b7789b34524faa2c2e97ba8ec923c3051
MGautier <moisesgautiergomez@correo.ugr.es>
2016-04-24 23:26:24 +0200

    Visualizations have event onclick. That event show a list of events iptables generated by system along the week

--- trunk/version-1-0/webapp/secproject/static/js/indexReact.js
+++ trunk/version-1-0/webapp/secproject/static/js/indexReact.js
@@ -1,141 +1,25 @@
-/*setTimeout(function () {
- c3.generate({
- data: {
- url: 'api/visualizations/week.json',
- mimeType: 'json',
- keys: {
- x: 'Name_Day',
- value: ['Process_Events']
- }
- },
- axis: {
- x: {
- type: 'category'
- }
- }
- });
- }, 1000);*/
-
-/*var chart = c3.generate({
- data: {
- url: [
- ['data1', 300, 350, 300, 0, 0, 0],
- ['data2', 130, 100, 140, 200, 150, 50]
- ],
- types: {
- data1: 'area',
- data2: 'area-spline'
- }
- }
- });*/
-
-
-/*var Visualization = React.createClass({
- render: function() {
- return (
- <div className="visualization">
- <div className="visualizationDate">
- {this.props.Date}
- </div>
- <div className="visualizationDay">
- {this.props.Day}
- </div>
- <div className="visualizationHour">
- {this.props.Hour}
- </div>
- <div className="visualizationEvents">
- {this.props.Events}
- </div>
- </div>
- );
- }
- });*/
-
-var web = "api/events/day/";
-
 var Visualization = React.createClass({
-  render: function() {
-    return(
-
-      c3.generate({
-        data: {
-          url: 'api/visualizations/chart.json/',
-          mimeType: 'json',
-          selection: {
-            enabled: true
-          },
-          onclick: function (d, element){
-
-            var Parent = React.createClass({
-              render: function() {
-                return (
-                    <EventsComponent source={this.props.ID_Source} date={this.props.Date} url={web + this.props.ID_Source + "/" + this.props.Date} key={this.props.id} pollInterval={60000} />
-                  /*  <div className="parent">
-                    <div className="parentDate">
-                    {this.props.Date}
-                  </div>
-                    <div className="parentEvents">
-                    {this.props.Events}
-                  </div>
-                    <div className="parentDay">
-                    {this.props.Day}
-                  </div>
-                   </div>*/
-                );
-              }
-            });
-
-            var ParentComponent = React.createClass({
-              loadParentFromServer: function(){
-                $.ajax({
-                  url: this.props.url,
-                  dataType: 'json',
-                  cache: false,
-                  success: function(data) {
-                    this.setState({data: data});
-                  }.bind(this),
-                  error: function(xhr, status, err){
-                    console.error(this.props.url, status, err.toString());
-                  }.bind(this)
-                });
-              },
-              getInitialState: function(){
-                return {data: []};
-              },
-              componentDidMount: function(){
-                this.loadParentFromServer();
-                setInterval(this.loadParentFromServer, this.props.pollInterval);
-              },
-              render: function(){
-                var testStyle = { fontSize: '18px', marginRight: '20px' };
-                return (
-                    <div className="parentComponent" style={testStyle}>
-                    <h1>Parent</h1>
-                    <ParentList data={this.state.data} key={this.state.id}/>
-                    </div>
-                );
-              }
-            });
-
-            var ParentList = React.createClass({
-              render: function(){
-                console.log("ENTRO POR AQUÍ");
-                var count = 0;
-                var parentNodes = this.props.data.map(function(parent){
-                    return (<li><Parent ID_Source={parent.id_source} Date={parent.date} Events={parent.events} Day={parent.day} key={parent.id} /></li>);
-                });
-                console.log("PARENT NODES", parentNodes);
-                return (
-                    <div className="parentList">
-                    <ul>
-                    {parentNodes}
-                  </ul>
-                    </div>
-                );
-              }
-            });
-
-            var Event = React.createClass({
+  componentDidMount: function() {
+    var days = Array.from(this.props.set);
+    var source = this.props.data.ID_Source;
+    c3.generate({
+      data: {
+        url: 'api/visualizations/chart.json/',
+        mimeType: 'json',
+        selection: {
+          enabled: true
+        },
+        keys: {
+          x: 'day',
+          value: ['events']
+        },
+        names: {
+          events: 'Iptables Events'
+        },
+        onclick: function (d, element){
+          console.log("DAY ACTUAL",days[d.index]);
+          var events_per_day = "api/events/day/" + source + "/" + days[d.index];
+          var Event = React.createClass({
               render: function() {
                 return (
                     <div className="event">
@@ -160,7 +44,7 @@ var Visualization = React.createClass({
                     this.setState({data: data});
                   }.bind(this),
                   error: function(xhr, status, err){
-                    console.error(web + this.props.day + "/"+ this.props.date, status, err.toString());
+                    console.error(this.props.url, status, err.toString());
                   }.bind(this)
                 });
               },
@@ -176,7 +60,7 @@ var Visualization = React.createClass({
                 return (
                     <div className="eventsComponent" style={testStyle}>
                     <h1>Events</h1>
-                    <EventsList data={this.state.data} key={this.state.id}/>
+                    <EventsList data={this.state.data} key={this.props.id}/>
                     </div>
                 );
               }
@@ -199,32 +83,25 @@ var Visualization = React.createClass({
                 );
               }
             });
-
-            ReactDOM.render(
-                <ParentComponent url="api/visualizations/chart/" pollInterval={60000} />,
-                //<EventsComponent url="api/events/" pollInterval={10000} />,
+          ReactDOM.render(
+              <EventsComponent url={events_per_day} pollInterval={60000} />,
               document.getElementById('sub-content')
             );
-          },
-          keys: {
-            x: 'day',
-            value: ['events']
-          },
-          names: {
-            events: 'Iptables Events'
-          },
-          type: 'area-spline'
         },
-        subchart: {
-          show: true
-        },
-        axis: {
-          x: {
-            type: 'category'
-          }
+        type: 'area-spline'
+      },
+      subchart: {
+        show: true
+      },
+      axis: {
+        x: {
+          type: 'category'
         }
-      })
-    );
+      }
+    });
+  },
+  render: function() {
+    return(<div className="c3-chart"></div>);
   }
 });
 
@@ -262,10 +139,12 @@ var VisualizationsComponent = React.createClass({
 
 var VisualizationsList = React.createClass({
   render: function(){
+    var my_set = new Set();
     var visualizationNodes = this.props.data.map(function(visualization){
       return (
-          <Visualization Date={visualization.Date} Day={visualization.Name_Day} ID_Source={visualization.ID_Source} Hour={visualization.Hour} Events={visualization.Process_Events} key={visualization.id} />
-        //<li><Visualization data={visualization} key={visualization.id} /></li>
+        my_set.add(visualization.Date),
+          <Visualization data={visualization} set={my_set} key={visualization.id}/>
+        //<Visualization Date={visualization.Date} Day={visualization.Name_Day} ID_Source={visualization.ID_Source} Hour={visualization.Hour} Events={visualization.Process_Events} key={visualization.id} />
       );
     });
     return (
