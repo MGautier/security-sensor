24c85abcef393715d1204e2eda5de60e394058bb
MGautier <moisesgautiergomez@correo.ugr.es>
2016-06-26 20:17:26 +0200

    Añadiendo configuracion de fuente iptables y ejecucion de hilo iptables

--- trunk/version-1-0/webapp/secproject/controller.py
+++ trunk/version-1-0/webapp/secproject/controller.py
@@ -28,6 +28,7 @@ class Controller(object):
     source_info = {}
     parent_pid = str(os.getpid())
     child_pid = str(os.getpid())
+    name_thread = ""
 
     def get_source(self):
         return self.source
@@ -50,24 +51,114 @@ class Controller(object):
     def get_log_processing(self):
         return self.source_info['P']
 
+    def get_source_info(self):
+        return self.source_info
+
+    def get_info(self):
+        print "--------------------------------------------------"
+        print "Source --> ", self.get_source()
+        print "Parent pid --> ", self.get_parent_pid()
+        print "Child pid --> ", self.get_child_pid()
+        print "Type source --> ", self.get_type_source()
+        print "Model source --> ", self.get_model_source()
+        print "Configuration file --> ", self.get_configuration()
+        print "Log processing --> ", self.get_log_processing()
+        print "Thread name --> ", self.name_thread
+        print "--------------------------------------------------"
 
 class IptablesController(Controller):
 
-    source = "iptables"
+    def set_source_info(source_info):
+        print "--------------------------------------------------"
+        print "Introduce los parametros de la configuracion de la fuente - iptables"
+        print "Valores por defecto ----"
+        print "[1] Ruta procesamiento: \'/var/log/iptables.log\',"
+        print "[2] Configuración fuente: \'.kernel/conf/iptables-conf.conf\'"
+        print "[3] Salir de la configuración"
+        print "Si no quieres modificar el campo introduce Intro en la selección"
+        print "--------------------------------------------------"
+
+        choose = 0
+        path_source = ""
+        config_source = ""
+        set = {}
+
+        while choose != str(3):
+            print "--------------------------------------------------"
+            choose = str(raw_input('Introduce parámetro a modificar ([3] - Saltar este paso, [0] - Ayuda): '))
+            print "--------------------------------------------------"
+            if choose == str(0):
+                print "--------------------------------------------------"
+                print "Introduce los parametros de la configuracion de la fuente - iptables"
+                print "Valores por defecto ----"
+                print "[1] Ruta procesamiento: \'/var/log/iptables.log\',"
+                print "[2] Configuración fuente: \'.kernel/conf/iptables-conf.conf\'"
+                print "[3] Salir de la configuración"
+                print "--------------------------------------------------"
+            elif choose == str(1):
+                print "--------------------------------------------------"
+                path_source = str(raw_input('Ruta de procesamiento de logs Ejemplo(/var/log/iptables.log): '))
+                print "--------------------------------------------------"
+                if not source_info['P'] == path_source:
+                    if not path_source:
+                        set['P'] = path_source
+                else:
+                    set['P'] = source_info['P']
+
+            elif choose == str(2):
+                print "--------------------------------------------------"
+                config_source = str(raw_input('Archivo de configuración fuente Ejemplo(./kernel/conf/iptables-conf.conf): '))
+                print "--------------------------------------------------"
+                if not source_info['C'] == config_source:
+                    if not config_source:
+                        set['C'] = config_source
+                else:
+                    set['C'] = source_info['C']
+
+            elif choose == str(3):
+                if not path_source and not config_source:
+                    return source_info
+                else:
+                    set = {
+                        'T': source_info['T'],
+                        'M': source_info['M'],
+                        'P': path_source,
+                        'C': config_source
+                    }
+            elif choose:
+                pass
+
+        return set
+
+
     # Si hago uso del fork da el error del Runtime y ejecuta otro pid igual a este
     # si comento el fork funciona correctamente pero al finalizar este proceso Main
     # finaliza toda la ejecucion de procesamiento
 
+
     #child_pid = os.fork()
-    source_info = {'T': 'Firewall', 'M': 'iptables', 'P': '/var/log/iptables.log',
+    source = "iptables"
+    source_info = {'T': 'Firewall',
+                   'M': 'iptables',
+                   'P': '/var/log/iptables.log',
                    'C': './kernel/conf/iptables-conf.conf'}
     args = (1,)
-    thread_iptables = Iptables(
-        args=args,
-        source_info=source_info
-    )
-    thread_iptables.start()
-    print "HOLA HOLA : ", thread_iptables.getName()
+    settings = set_source_info(source_info)
+
+    if not settings == source_info:
+        source_info = settings
+
+    try:
+        thread_iptables = Iptables(
+            args=args,
+            source_info=source_info
+        )
+        thread_iptables.start()
+        name_thread = thread_iptables.getName()
+    except Exception as ex:
+        print "Exception --> ", ex
+
+
 
 
 class SnortController(Controller):
