625f2ea21dff39011ba3a4269f0afc29cd89ecc8
MGautier <moisesgautiergomez@correo.ugr.es>
2016-06-26 20:19:03 +0200

    Añadiendo comandos para prompt de interacción y obtención de información de fuente iptables

--- trunk/version-1-0/webapp/secproject/main.py
+++ trunk/version-1-0/webapp/secproject/main.py
@@ -1,6 +1,7 @@
 import signal
 import os
 import manager
+import re
 
 if __name__ == "__main__":
 
@@ -15,25 +16,43 @@ if __name__ == "__main__":
     print "IPTABLES - GET LOG: ", iptables.get_log_processing()
 
     if not iptables.get_child_pid() == iptables.get_parent_pid():
-        set_threads = [ iptables.get_child_pid(), iptables.get_parent_pid()  ]
+        threads = { 'Main thread' : iptables.get_parent_pid(), 'Thread-1' : iptables.get_child_pid() }
     else:
-        set_threads = [ iptables.get_parent_pid() ]
+        threads = { 'Main thread' : iptables.get_parent_pid() }
 
     while True:
 
-        data_input = raw_input(' ')
-        print "SET-THREADS: ", set_threads
+        data_input = raw_input('> ')
 
         if data_input == 'exit':
-            for pid in set_threads:
+            for pid in threads:
                 # Primero matamos al proceso hijo y luego al padre
                 # para que el hijo no entre en inanicion
-                print "Matando al proceso: ", pid
-                os.kill(int(pid),signal.SIGKILL)
+                print "--------------------------------------------------"
+                print "Matando al proceso: ", int(threads[pid])
+                os.kill(int(threads[pid]),signal.SIGKILL)
             os._exit(0)
-        elif data_input in set_threads:
-            print "Matando al proceso: ", data_input
-            os.kill(int(data_input), signal.SIGKILL)
-
-        print "DATA INPUT: ", data_input
-        print "TYPE DATE INPUT: ", type(data_input)
+        elif data_input == 'commands':
+            print "-------------------------------------------------"
+            print "info -> Informacion sobre la fuente en ejecucion "
+            print "clear -> Limpia la pantalla de informacion "
+            print "pids -> Muestra los pids asociados en ejecucion "
+            print "kill PID (valor) -> Mata al PID que se introduzca (Si es el padre aborta la ejecucion) "
+            print "exit -> Aborta la ejecucion del proceso y lo mata."
+            print "--------------------------------------------------"
+        elif data_input == 'info':
+            iptables.get_info()
+        elif data_input == 'clear':
+            os.system('clear')
+        elif data_input == 'pids':
+            for it_thread in threads:
+                print " " + it_thread + " --> " + threads[it_thread]
+        elif "kill" in data_input:
+            print "--------------------------------------------------"
+            pid = re.split("\W?", data_input)[1]
+            print "Matando al proceso: ", pid
+            os.kill(int(pid), signal.SIGKILL)
+        else:
+            print "--------------------------------------------------"
+            print "Opcion " + data_input + " incorrecta (commands para mas informacion)"
+            print "--------------------------------------------------"
