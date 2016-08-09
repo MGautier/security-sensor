eed0a4558b3f0035c8d32e1191ccb661c06bba1e
MGautier <moisesgautiergomez@correo.ugr.es>
2016-08-09 20:54:01 +0200

    Añadiendo interacción de ajax para la gráfica de eventos en tiempo real

--- trunk/version-1-0/webapp/secproject/secapp/static/js/extra-functions.js
+++ trunk/version-1-0/webapp/secproject/secapp/static/js/extra-functions.js
@@ -192,12 +192,46 @@ $(function () {
             // set up the updating of the chart each second
 
             var series = this.series[0];
-            setInterval(function () {
-              var x = (new Date()).getTime(), // current time
-                  y = ajax_data[0].Events;
 
-              series.addPoint([x, y], true, true);
-            }, 2500);
+            setTimeout(function(){
+              setInterval(function () {
+                var x = (new Date()).getTime(); // current time
+
+                var y;
+                if ( ajax_data.length == 0 )
+                {
+                  y = 0;
+                }else{
+                  y = ajax_data[0].Events;
+                }
+
+                series.addPoint([x, y], true, true);
+                series.show();
+
+              }, 2700);
+
+              series.setData(
+                (function () {
+                  // generate an array of random data
+                  var data = [],
+                      time = (new Date()).getTime(),
+                      i;
+                  for (i = -19; i <= 0; i += 1) {
+                    data.push({
+                      x: time + i * 1000,
+                      y: Math.random()
+                    });
+                    console.log("X: ", time + i * 1000);
+                    console.log("Y: ", Math.random());
+
+                  }
+
+                  return data;
+                }())
+              );
+
+            }, 2000);
+
           }
         }
       },
@@ -236,20 +270,7 @@ $(function () {
       },
       series: [{
         name: 'Events Iptables',
-        data: (function () {
-          // generate an array of random data
-          var data = [],
-              time = (new Date()).getTime(),
-              i;
-
-          for (i = -19; i <= 0; i += 1) {
-            data.push({
-              x: time + i * 1000,
-              y: Math.random()
-            });
-          }
-          return data;
-        }())
+        data: []
       }]
     });
   });
